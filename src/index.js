526a20d334cd6ee72c0709d926e4ebeabdd8bc3c
Masamune Hiroiwa <rider_jump_kick@yahoo.co.jp>
2020-05-25 23:14:55 +0900

    reduxをインストール。Store.jsを作成.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import MemoStore from './memo/Store'
+// import * as serviceWorker from './serviceWorker';
 
+// 表示をレンダリング
 ReactDOM.render(
-  <React.StrictMode>
+  < Provider store = {MemoStore}>
     <App />
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById('root')
 );
 
